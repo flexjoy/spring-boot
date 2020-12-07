1916a7815b46af842f7c31a9e6ce27728c381776
scherepanov <scherepanov@neoflex.ru>
2020-12-07 13:00:50 +0300

    Add logging

--- src/main/java/com/example/sprinboottest/TestController.java
+++ src/main/java/com/example/sprinboottest/TestController.java
@@ -1,13 +1,18 @@
 package com.example.sprinboottest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+
+@Slf4j
 @RestController
 public class TestController {
 
     @GetMapping
     public String greet() {
+        log.info("Hit {}", Instant.now());
         return "Hello-from-Openshift";
     }
 }
