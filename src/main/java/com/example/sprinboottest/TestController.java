d17566a4963e3e04ac675aafc30620460980947a
scherepanov <scherepanov@neoflex.ru>
2020-12-07 12:25:03 +0300

    Initial commit

---
+++ src/main/java/com/example/sprinboottest/TestController.java
@@ -0,0 +1,13 @@
+package com.example.sprinboottest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String greet() {
+        return "Hello-from-Openshift";
+    }
+}
